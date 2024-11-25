5fe75f5f308d2d9b81b1b91f95f7034bdcc1a6e2
Akhirah Strong <codeclubdetroit@gmail.com>
2024-11-25 15:12:49 -0500

    Comment

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 const PersonalRoom = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
+      {/* Next up latest */}
       <h1 className="text-3xl font-bold">Personal Room</h1>
     </section>
   );
